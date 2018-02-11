3abf64f5f9e6e86cf376dd42349fa96e61e857ff
waqar amjad <waqaramjad345@gmail.com>
2018-02-11 12:35:38 +0500

    deleting forrm

--- src/components/StudentPanel/student.js
+++ src/components/StudentPanel/student.js
@@ -59,28 +59,27 @@ class student extends Component {
 	<div className="row">
 	  <form role="form">
         <div className="form-group col-xs-10 col-sm-4 col-md-4 col-lg-4">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="exampleInputEmail1">Email address</label>
             <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
         </div>
         <div className="form-group col-xs-10 col-sm-4 col-md-4 col-lg-4">
-            <label for="exampleInputEmail1">Name</label>
+            <label htmlFor="exampleInputEmail1">Name</label>
             <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Enter Name"/>
         </div>
         <div className="clearfix"></div>
         <div className="form-group col-xs-10 col-sm-4 col-md-4 col-lg-4">
-            <label for="exampleInputPassword1">Password</label>
+            <label htmlFor="exampleInputPassword1">Password</label>
             <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
         </div>
         <div className="form-group col-xs-10 col-sm-4 col-md-4 col-lg-4">
-            <label for="exampleInputPassword1">Confirm Password</label>
+            <label htmlFor="exampleInputPassword1">Confirm Password</label>
             <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Confirm Password"/>
         </div>
-        <div className="clearfix"></div>
-        <div className="form-group col-xs-10 col-sm-10 col-md-4 col-lg-4">
-            <label for="exampleInputFile">File input</label>
-            <input type="file" id="exampleInputFile"/>
-            <p className="help-block">Example block-level help text here.</p>
+        <div className="form-group col-xs-10 col-sm-4 col-md-4 col-lg-4">
+            <label htmlFor="exampleInputPassword1">university</label>
+            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Confirm Password"/>
         </div>
+        <div className="clearfix"></div>
         <div className="clearfix"></div>
         <div className="col-xs-10 col-sm-4 col-md-4 col-lg-4">
             <button type="submit" className="btn btn-default">Submit</button>
